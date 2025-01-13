9c8c92bb087b2b955d592dea8c1bef324ba219a4
amel fradj <amelfradj1@gmail.com>
2025-01-13 19:52:40 +0100

    Update

--- src/Kernel.php
+++ src/Kernel.php
@@ -3,9 +3,28 @@
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Bundle\DebugBundle\DebugBundle; // Assurez-vous que DebugBundle est utilisé ici
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    protected function configureBundles(): iterable
+    {
+        $bundles = [
+            Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+        ];
+
+        // Inclure DebugBundle uniquement en dev/test
+        if ($this->getEnvironment() === 'dev' || $this->getEnvironment() === 'test') {
+            $bundles[DebugBundle::class] = ['dev' => true, 'test' => true];
+        }
+
+        foreach ($bundles as $class => $envs) {
+            if (isset($envs['all']) || isset($envs[$this->getEnvironment()])) {
+                yield new $class();
+            }
+        }
+    }
 }
